58da5da73218dec26ae53bd15517e96904320d7c
tomk0 <tomkelly16111996@gmail.com>
2020-03-21 17:13:02 +0000

    Extracted Countary data

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import pandas as pd
 import numpy as np
+import matplotlib as mp
 
 
 def main():
-    print("Hello world")
+    full_data = pd.read_csv('full_data.csv')
+    china = full_data.loc[full_data['location'] == 'China']
+    uk = full_data.loc[full_data['location'] == 'United Kingdom']
+    italy = full_data.loc[full_data['location'] == 'Italy']
+
+
 
 
 if __name__ == "__main__":
